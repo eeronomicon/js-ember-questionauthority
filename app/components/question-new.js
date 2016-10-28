c454a16ac626fa37b6621ec024a55fd06601ab63
HK Kahng <hk@router-rooter.com>
2016-10-28 09:25:34 -0700

    add component to have user submit new questions

---
+++ app/components/question-new.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  displayQuestionForm : false,
+  actions: {
+    showQuestionForm() {
+      this.set('displayQuestionForm', true);
+    },
+    newQuestion() {
+      var params = {
+        author: this.get('author') ? this.get('author') : "",
+        question: this.get('question') ? this.get('question') : "",
+        notes: this.get('notes') ? this.get('notes') : ""
+      };
+      this.sendAction('saveQuestion', params);
+      this.set('displayQuestionForm', false);
+    }
+  }
+});
