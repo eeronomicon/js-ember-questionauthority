bf9a213d7b7c955030ef09822bde2938537e9024
HK Kahng <hk@router-rooter.com>
2016-10-28 10:41:45 -0700

    add component and template to add Answers to a Question

---
+++ app/components/answer-new.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  displayAnswerForm: false,
+  actions: {
+    showAnswerForm() {
+      this.set('displayAnswerForm', true);
+    },
+    hideAnswerForm() {
+      this.set('displayAnswerForm', false);
+    },
+    newAnswer() {
+      var params = {
+        author: this.get('author'),
+        answer: this.get('answer'),
+        question: this.get('questionToAnswer')
+      };
+      this.set('displayAnswerForm', false);
+      this.sendAction('saveAnswer', params);
+    }
+  }
+});
--- app/routes/question.js
+++ app/routes/question.js
@@ -13,6 +13,17 @@ export default Ember.Route.extend({
       });
       question.save();
       this.transitionTo('question', question.id);
+    },
+
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function() {
+        return question.save();
+      });
+      this.transitionTo('index');
     }
+
   }
 });
