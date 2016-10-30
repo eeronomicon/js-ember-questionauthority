2031366ef735c9baf0a7f8682be4ead231f85cf4
HK Kahng <hk@router-rooter.com>
2016-10-29 21:18:58 -0700

    update Answers list component to sort scores in descending order

--- app/components/answer-list.js
+++ app/components/answer-list.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortBy: ['score:desc'],
+  sortedAnswers: Ember.computed.sort('answersList', 'sortBy'),
   actions: {
     voteAnswer(answer, adjustment) {
       this.sendAction('voteAnswer', answer, adjustment);
